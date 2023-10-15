5bade241a02e54f64d4b4b2219560d1d7c78ffcc
EnzeXu <xue20@wfu.edu>
2023-10-15 10:12:19 -0400

    updates

--- test_run.py
+++ test_run.py
@@ -79,6 +79,7 @@ def main():
             print(output_y.shape)
             print(output_y[:5])
             print(y_labels[:])
+            print(output_y.shape[-1])
             assert ((0 <= y_labels) & (y_labels < output_y.shape[-1])).all()
             loss = loss_fun(output_y, y_labels)
             print(loss)
